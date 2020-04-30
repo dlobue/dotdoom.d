35ff68b5a8a9de041f56c6620abee8f1df38865b
Dominic LoBue <dominic.lobue@gmail.com>
2020-04-30 14:08:09 -0700

    remove org-notion -- it doesn't work

--- packages.el
+++ packages.el
@@ -27,8 +27,8 @@
 ;(package! another-package
 ;  :recipe (:host github :repo "username/repo"))
 
-(package! org-notion
-  :recipe (:host github :repo "RadekMolenda/org-notion"))
+; (package! org-notion
+;   :recipe (:host github :repo "RadekMolenda/org-notion"))
 
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
