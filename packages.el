e65ae85bb2199d35ca7b64f88a40a71bcbab3b0d
Dominic LoBue <dominic.lobue@gmail.com>
2021-09-29 17:13:28 -0700

    Update add recipe for ejira

--- packages.el
+++ packages.el
@@ -22,7 +22,12 @@
 ; (package! esup)
 
 ;; ejira
-; (package! ejira)
+; (package! ejira :recipe
+;   (:host github
+;    :repo "nyyManni/ejira"
+;    :files ("*.el")
+;    )
+;   )
 
 ;; To install a package directly from a particular repo, you'll need to specify
 ;; a `:recipe'. You'll find documentation on what `:recipe' accepts here:
