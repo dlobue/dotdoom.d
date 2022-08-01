842b8ec9b5c3e755aaa6f2b707037873bd582b55
Dominic LoBue <dominic.lobue@gmail.com>
2022-08-01 14:50:12 -0700

    update mail download to refresh inbox when new mail is received

--- override/prodigy-mail.el
+++ override/prodigy-mail.el
@@ -4,15 +4,15 @@
     :command "goimapnotify"
     :args '("-conf" ".config/imapnotify/scalyr.conf")
     :cwd (expand-file-name "~")
-    :tags '(mbsync email work scalyr)
-    ; :kill-process-buffer-on-stop t
-    )
+    :tags '(mbsync email work scalyr))
+
   (prodigy-define-service
     :name "mailsync-sentinelone"
     :command "goimapnotify"
     :args '("-conf" ".config/imapnotify/sentinelone.conf")
     :cwd (expand-file-name "~")
     :tags '(mbsync email work sentinelone)
-    ; :kill-process-buffer-on-stop t
-    )
-)
+    :on-output (lambda (&rest args)
+                 (let ((output (plist-get args :output)))
+                   (when (s-matches? "refresh notmuch" output)
+                     (notmuch-refresh-all-buffers))))))
