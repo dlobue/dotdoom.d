16f82c992ecb15f1f35b48e67bee208cddfcd9b6
Dominic LoBue <dominic.lobue@gmail.com>
2023-04-28 12:04:13 -0700

    config cleanup and standardization of code commenting

--- config.el
+++ config.el
@@ -32,16 +32,50 @@
       doom-unicode-font (font-spec :family "Source Code Pro")
       doom-big-font (font-spec :family "Source Code Pro"))
 
-;(setq frame-background-mode "dark")
-;(disable-theme 'zenburn)
-(add-to-list 'custom-theme-load-path (expand-file-name "themes" doom-private-dir))
-(add-to-list 'load-path (expand-file-name "themes" doom-private-dir))
+;; (setq frame-background-mode "dark")
+;; (disable-theme 'zenburn)
+;; (add-to-list 'custom-theme-load-path (expand-file-name "themes" doom-private-dir))
+;; (add-to-list 'load-path (expand-file-name "themes" doom-private-dir))
 ;; Uncomment this to increase font size
 ;; (set-face-attribute 'default nil :height 140)
-;(load-theme 'black-mustang t) ;; gvim theme
-;(load-theme 'noctilux t) ;; lighttable theme
+;; (load-theme 'black-mustang t) ;; gvim theme
+;; (load-theme 'noctilux t) ;; lighttable theme
+;; (load-theme 'bubbleberry t) ;; lighttable theme
 
-; (load-theme 'bubbleberry t) ;; lighttable theme
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/docs/notes/")
+
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
